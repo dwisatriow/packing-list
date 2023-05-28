8fb6817e5e37d30859371e441273eac03e3c1039
dwisatriow <dwisatrio.dev@gmail.com>
2023-05-28 23:03:52 +0700

    useMemo and useCallback

--- src/components/application.js
+++ src/components/application.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import {
   createItem,
   filterItems,
@@ -14,25 +14,34 @@ import NewItem from './new-item';
 const Application = () => {
   const [items, setItems] = useState(getInitialItems());
 
-  const add = (name) => {
-    const item = createItem(name);
-    setItems([...items, item]);
-  };
+  const add = useCallback(
+    (name) => {
+      const item = createItem(name);
+      setItems([...items, item]);
+    },
+    [items],
+  );
 
-  const update = (id, updates) => {
-    setItems(updateItem(items, id, updates));
-  };
+  const update = useCallback(
+    (id, updates) => {
+      setItems(updateItem(items, id, updates));
+    },
+    [items],
+  );
 
-  const remove = (id) => {
-    setItems(removeItem(items, id));
-  };
+  const remove = useCallback(
+    (id) => {
+      setItems(removeItem(items, id));
+    },
+    [items],
+  );
 
   const unpackedItems = filterItems(items, { packed: false });
   const packedItems = filterItems(items, { packed: true });
 
-  const markAllAsUnpacked = () => {
+  const markAllAsUnpacked = useCallback(() => {
     return setItems(items.map((item) => ({ ...item, packed: false })));
-  };
+  }, [items]);
 
   return (
     <main className="mx-auto flex flex-col gap-8 p-8 lg:max-w-4xl">
